3a1656ab3ccaebe841a90a85f9b3441d5b51191c
alex kerendian <akerendian@tablethotels.com>
2019-06-13 17:22:11 -0400

    Initial Commit

---
+++ CalendarView/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CalendarView
+//
+//  Created by Alex Kerendian on 6/13/19.
+//  Copyright © 2019 Alexander Kerendian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
