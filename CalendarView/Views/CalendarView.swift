557d42b5fcac2717879813e60305e15cf24bf4de
ak <atrix113@aol.com>
2019-06-18 07:33:53 -0400

    replaces content view with calendar view as the root view

--- CalendarView/Views/CalendarView.swift
+++ CalendarView/Views/CalendarView.swift
@@ -1,14 +1,14 @@
 //
-//  ContentView.swift
+//  CalendarView.swift
 //  CalendarView
 //
-//  Created by Alex Kerendian on 6/13/19.
+//  Created by Alex Kerendian on 6/18/19.
 //  Copyright © 2019 Alexander Kerendian. All rights reserved.
 //
 
 import SwiftUI
 
-struct ContentView : View {
+struct CalendarView : View {
     var body: some View {
         VStack (alignment: .leading, spacing: 0) {
             WeekdayHeaderView()
@@ -18,16 +18,16 @@ struct ContentView : View {
                         MonthView()
                     }
                     Divider().background(Color.black)
-                }.background(Color.black)
+                    }.background(Color.black)
             }
         }.background(Color.gray)
     }
 }
 
 #if DEBUG
-struct ContentView_Previews : PreviewProvider {
+struct CalendarView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        CalendarView()
     }
 }
 #endif
